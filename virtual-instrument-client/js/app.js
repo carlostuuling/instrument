348ce4b3e6d4e8def762b3ddf36be1ab2f7553c2
Katzyh <kejja15@gmail.com>
2021-12-15 09:10:15 +0200

    favicon

--- virtual-instrument-client/js/app.js
+++ virtual-instrument-client/js/app.js
@@ -1,6 +1,6 @@
 // Socket.io server configuration
 // const socket = io("http://localhost:8000");
-const socket = io("http://10.25.185.88:8000");
+const socket = io("http://10.25.184.66:8000/");
 
 socket.on("connect", () => {
   console.log("Client connected to: " + socket.id);
@@ -8,9 +8,9 @@ socket.on("connect", () => {
 });
 
 socket.on('send-data', res => {
-  recieveData(res); 
+  recieveData(res);
 });
 
 socket.on("disconnect", () => {
-  console.log("Client disconnected from" + socket.id); 
+  console.log("Client disconnected from" + socket.id);
 });
